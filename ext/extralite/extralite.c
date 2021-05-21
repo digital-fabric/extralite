4368d65657dabe976ef0e2b87fb5832426aa6155
Sharon Rosner <sharon@noteflakes.com>
2021-05-21 13:52:28 +0000

    Implement rough sketch for yielding row as hashes

--- ext/extralite/extralite.c
+++ ext/extralite/extralite.c
@@ -51,37 +51,84 @@ VALUE Database_initialize(VALUE self, VALUE path) {
   return Qnil;
 }
 
-typedef struct ExecuteContext_t {
-  VALUE block;
-} ExecuteContext_t;
-
-static int callback_block_hash(void *ctx, int argc, char **argv, char **azColName) {
-  // ExecuteContext_t *execute_context = ctx;
-  
-  VALUE hash = rb_hash_new();
-  for (int i=0; i < argc; i++) {
-    rb_hash_aset(hash, rb_str_new_cstr(azColName[i]), rb_str_new_cstr(argv[i]));
-    // printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+inline VALUE get_column_value(sqlite3_stmt *stmt, int col, int type) {
+  fprintf(stderr, "get_column_value col=%d type=%d\n", col, type);
+  switch (type) {
+    case SQLITE_INTEGER:
+      return INT2NUM(sqlite3_column_int(stmt, col));
+    case SQLITE_FLOAT:
+      return DBL2NUM(sqlite3_column_double(stmt, col));
+    case SQLITE_TEXT:
+      return rb_str_new_cstr((char *)sqlite3_column_text(stmt, col));
+    case SQLITE_BLOB:
+      // TODO: implement
+      return Qfalse;
+    case SQLITE_NULL:
+      return Qnil;
+    default:
+      // TODO: raise error
+      return Qfalse;
   }
-  // printf("\n");
-  rb_yield(hash);
-  RB_GC_GUARD(hash);
-  return 0;
 }
 
 VALUE Database_execute(VALUE self, VALUE sql) {
   int rc;
-  char *zErrMsg = 0;
-  ExecuteContext_t ctx;
+  sqlite3_stmt* stmt;
+  int column_count;
   Database_t *db;
   GetDatabase(self, db);
 
-  ctx.block = rb_block_given_p() ? rb_block_proc() : Qnil;
-  rc = sqlite3_exec(db->sqlite3_db, StringValueCStr(sql), callback_block_hash, &ctx, &zErrMsg);
-  if (rc != SQLITE_OK) {
-    fprintf(stderr, "SQL error: %s\n", zErrMsg);
-    sqlite3_free(zErrMsg);
+  rc = sqlite3_prepare(db->sqlite3_db, RSTRING_PTR(sql), RSTRING_LEN(sql), &stmt, 0);
+  if (rc) {
+    fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db->sqlite3_db));
+    sqlite3_finalize(stmt);
+    // TODO: raise error
+    return Qfalse;
   }
+
+  column_count = sqlite3_column_count(stmt);
+  if (column_count > 0) {
+    VALUE hash;
+    VALUE column_names = rb_ary_new2(column_count);
+    for (int i = 0; i < column_count; i++) { 
+      VALUE name = ID2SYM(rb_intern(sqlite3_column_name(stmt, i)));
+      rb_ary_push(column_names, name);
+    }
+
+    hash = rb_hash_new();
+    while (1) {
+      rc = sqlite3_step(stmt);
+      fprintf(stderr, "step rc=%d\n", rc);
+      if (rc == SQLITE_ROW) {
+        rb_hash_clear(hash);
+        for (int i = 0; i < column_count; i++) {
+          VALUE value = get_column_value(stmt, i, sqlite3_column_type(stmt, i));
+          rb_hash_aset(hash, RARRAY_AREF(column_names, i), value);
+        }
+        rb_yield(hash);
+      }
+      break;
+      switch (rc) {
+        case SQLITE_DONE:
+          break;
+        case SQLITE_BUSY:
+          break;
+        case SQLITE_ERROR:
+          fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db->sqlite3_db));
+          break;
+        default:
+          fprintf(stderr, "rc = %d\n", rc);
+          break;
+      }
+
+    }
+
+    RB_GC_GUARD(column_names);
+    RB_GC_GUARD(hash);
+  }
+
+  sqlite3_finalize(stmt);
+
   return Qnil;
 }
 
