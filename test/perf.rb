c9ecc82de89d9f9ff63dad7601ab69b35132c0bc
Sharon Rosner <sharon@noteflakes.com>
2021-12-13 08:29:04 +0100

    Benchmark different table sizes

--- test/perf.rb
+++ test/perf.rb
@@ -13,36 +13,39 @@ require 'benchmark/ips'
 require 'fileutils'
 
 DB_PATH = '/tmp/extralite_sqlite3_perf.db'
-COUNT = 10000
 
-def prepare_database
+def prepare_database(count)
   FileUtils.rm(DB_PATH) rescue nil
   db = Extralite::Database.new(DB_PATH)
   db.query('create table foo ( a integer primary key, b text )')
   db.query('begin')
-  COUNT.times { db.query('insert into foo (b) values (?)', "hello#{rand(1000)}" )}
+  count.times { db.query('insert into foo (b) values (?)', "hello#{rand(1000)}" )}
   db.query('commit')
 end
 
-def sqlite3_run
+def sqlite3_run(count)
   db = SQLite3::Database.new(DB_PATH, :results_as_hash => true)
   results = db.execute('select * from foo')
-  raise unless results.size == COUNT
+  raise unless results.size == count
 end
 
-def extralite_run
+def extralite_run(count)
   db = Extralite::Database.new(DB_PATH)
   results = db.query('select * from foo')
-  raise unless results.size == COUNT
+  raise unless results.size == count
 end
 
-prepare_database
+[10, 1000, 100000].each do |c|
+  puts; puts; puts "Record count: #{c}"
 
-Benchmark.ips do |x|
-  x.config(:time => 3, :warmup => 1)
+  prepare_database(c)
 
-  x.report("sqlite3") { sqlite3_run }
-  x.report("extralite") { extralite_run }
+  Benchmark.ips do |x|
+    x.config(:time => 3, :warmup => 1)
 
-  x.compare!
+    x.report("sqlite3") { sqlite3_run(c) }
+    x.report("extralite") { extralite_run(c) }
+
+    x.compare!
+  end
 end
