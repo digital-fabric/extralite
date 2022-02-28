f9b583fd49fe8f3a29c0993635f15ab94bf7ac08
Sharon Rosner <sharon@noteflakes.com>
2022-02-28 08:55:38 +0100

    Fix test_sqlite3_version, do not expect specific version

--- test/test_extralite.rb
+++ test/test_extralite.rb
@@ -4,10 +4,6 @@ require_relative 'helper'
 
 class ExtraliteTest < MiniTest::Test
   def test_sqlite3_version
-    reported = `sqlite3 --version` rescue ''
-    match = reported.match(/^([\d\.]+)/)
-    if match
-      assert_equal match[1], Extralite.sqlite3_version
-    end
+    assert_match /^3\.\d+\.\d+$/, Extralite.sqlite3_version
   end
 end
