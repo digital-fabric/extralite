e2111ad9075a5d242d9210aa9def4255c28e7954
Sharon Rosner <sharon@noteflakes.com>
2022-02-28 08:49:17 +0100

    Fix sqlite3 version test

--- test/test_extralite.rb
+++ test/test_extralite.rb
@@ -3,12 +3,11 @@
 require_relative 'helper'
 
 class ExtraliteTest < MiniTest::Test
-  SQLITE3_C_PATH = File.expand_path('../ext/extralite/sqlite3.c', __dir__)
-  SQLITE_VERSION_DEFINE_REGEXP = /#define SQLITE_VERSION\s+"([\d\.]+)"/m.freeze
-
   def test_sqlite3_version
-    version = IO.read(SQLITE3_C_PATH).match(SQLITE_VERSION_DEFINE_REGEXP)[1]
-
-    assert_equal version, Extralite.sqlite3_version
+    reported = `sqlite3 --version` rescue ''
+    match = reported.match(/^([\d\.]+)/)
+    if match
+      assert_equal match[1], Extralite.sqlite3_version
+    end
   end
 end
