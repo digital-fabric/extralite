a1e8d2bd19eedd412d17b3a4bc1c7d3eb26f4976
Sharon Rosner <sharon@noteflakes.com>
2023-01-27 07:03:47 +0100

    Fix compiling with bundled sqlite (some defines were missing)
    Also, add a `EXTRALITE_BUNDLE` env var for forcing bundle compilation

--- ext/extralite/extconf-bundle.rb
+++ ext/extralite/extconf-bundle.rb
@@ -2,8 +2,15 @@
 
 require 'mkmf'
 
-$CFLAGS << " -Wno-undef"
-$CFLAGS << " -Wno-discarded-qualifiers"
+$CFLAGS << ' -Wno-undef'
+$CFLAGS << ' -Wno-discarded-qualifiers'
+$CFLAGS << ' -Wno-unused-function'
+
+$defs << "-DHAVE_SQLITE3_ENABLE_LOAD_EXTENSION"
+$defs << "-DHAVE_SQLITE3_LOAD_EXTENSION"
+$defs << "-DHAVE_SQLITE3_ERROR_OFFSET"
+
+have_func('usleep')
 
 dir_config('extralite_ext')
 create_makefile('extralite_ext')
