6cb11781bcb844db333e791d86d8e2aadd6b1fea
Sharon Rosner <sharon@noteflakes.com>
2024-01-22 15:14:31 +0100

    Implement Changeset#to_a

--- test/test_changeset.rb
+++ test/test_changeset.rb
@@ -45,6 +45,35 @@ class ChangesetTest < MiniTest::Test
     assert_equal [
       [:delete, 't', [1, 22, 3], nil]
     ], changes
+  end
 
+  def test_to_a
+    changeset = Extralite::Changeset.new
+
+    changeset.track(@db, [nil]) do
+      @db.execute('insert into t values (1, 2, 3)')
+      @db.execute('insert into t values (4, 5, 6)')
     end
+    assert_equal [
+      [:insert, 't', nil, [1, 2, 3]],
+      [:insert, 't', nil, [4, 5, 6]]
+    ], changeset.to_a
+
+
+    changeset.track(@db, [nil]) do
+      @db.execute('update t set y = 22.22 where z < 10')
+    end
+    assert_equal [
+      [:update, 't', [1, 2, nil], [nil, 22.22, nil]],
+      [:update, 't', [4, 5, nil], [nil, 22.22, nil]]
+    ], changeset.to_a
+
+
+    changeset.track(@db, [nil]) do
+      @db.execute('delete from t where x = 1')
+    end
+    assert_equal [
+      [:delete, 't', [1, 22.22, 3], nil]
+    ], changeset.to_a
+  end
 end
