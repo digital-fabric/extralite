c27d56264475acf4c6f3c88efb946a24017fd4c4
Sharon Rosner <sharon@noteflakes.com>
2026-06-29 09:59:50 +0200

    Add many-to-many tests

--- test/test_transform.rb
+++ test/test_transform.rb
@@ -212,3 +212,111 @@ class TransformOneToManyTest < Minitest::Test
     ], result[1][:comments]
   end
 end
+
+class TransformManyToManyTest < Minitest::Test
+  def setup
+    @db = Extralite::Database.new(':memory:')
+    @db.pragma('foreign_keys' => 1)
+    @db.execute <<~SQL
+      create table posts (
+        id integer primary key,
+        title text,
+        content text
+      );
+      create table tags (
+        id integer primary key,
+        name text
+      );
+      create table posts_tags (
+        post_id integer references posts(id) on delete cascade,
+        tag_id integer references tags(id) on delete cascade
+      );
+
+      insert into posts (title, content) values ('T1', 'C1');
+      insert into posts (title, content) values ('T2', 'C2');
+      insert into tags (name) values ('tag1');
+      insert into tags (name) values ('tag2');
+      insert into tags (name) values ('tag3');
+
+      insert into posts_tags(post_id, tag_id) values (1, 1);
+      insert into posts_tags(post_id, tag_id) values (1, 2);
+      insert into posts_tags(post_id, tag_id) values (2, 2);
+      insert into posts_tags(post_id, tag_id) values (2, 3);
+    SQL
+
+    @sql = <<~SQL
+      select
+        posts.id, posts.title, posts.content,
+        tags.id, tags.name
+      from posts
+      left outer join posts_tags on posts_tags.post_id = posts.id
+      left outer join tags on posts_tags.tag_id = tags.id
+      order by posts.id, tags.id
+    SQL
+
+    @transform = {
+      identity_idx: 0,
+      columns: [
+        :id,
+        :title,
+        :content,
+        [{
+          name: :tags,
+          identity_idx: 3,
+          columns: [:id, :name]
+        }]
+      ]
+    }
+  end
+
+  def test_transform_many_to_many_to_h
+    t = Extralite::Transform.new(@transform)
+    assert_equal @transform, t.to_h
+  end
+
+  def test_transform_many_to_many_query
+    t = Extralite::Transform.new(@transform)
+    result = @db.query(t, @sql)
+    assert_kind_of Array, result
+    assert_equal 2, result.size
+
+    assert_equal 'T1', result[0][:title]
+    assert_equal 'C1', result[0][:content]
+    assert_equal [
+      { id: 1, name: 'tag1' },
+      { id: 2, name: 'tag2' }
+    ], result[0][:tags]
+
+    assert_equal 'T2', result[1][:title]
+    assert_equal 'C2', result[1][:content]
+    assert_equal [
+      { id: 2, name: 'tag2' },
+      { id: 3, name: 'tag3' },
+    ], result[1][:tags]
+    assert_equal result[0][:tags][1].object_id, result[1][:tags][0].object_id
+  end
+
+  def test_transform_many_to_many_query_with_block
+    t = Extralite::Transform.new(@transform)
+    result = []
+    ret = @db.query(t, @sql) { result << it }
+    assert_equal @db,  ret
+    assert_kind_of Array, result
+    assert_equal 2, result.size
+
+    assert_equal 'T1', result[0][:title]
+    assert_equal 'C1', result[0][:content]
+    assert_equal [
+      { id: 1, name: 'tag1' },
+      { id: 2, name: 'tag2' }
+    ], result[0][:tags]
+
+    assert_equal 'T2', result[1][:title]
+    assert_equal 'C2', result[1][:content]
+    assert_equal [
+      { id: 2, name: 'tag2' },
+      { id: 3, name: 'tag3' },
+    ], result[1][:tags]
+    assert_equal result[0][:tags][1].object_id, result[1][:tags][0].object_id
+  end
+end
